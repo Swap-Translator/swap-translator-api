6d7888ac80eaa4bd9e51ede305d6a363e72d4a1d
albqvictor1508 <albq.victor@gmail.com>
2025-05-31 11:25:33 -0300

    perf: remove unused imports

--- src/main/java/com/swap/api/dto/TranslateRequest.java
+++ src/main/java/com/swap/api/dto/TranslateRequest.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
 
 @Getter
 @AllArgsConstructor
