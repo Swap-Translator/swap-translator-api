5990948fb67d4de239780bfb13d28792d2c348c3
albqvictor1508 <albq.victor@gmail.com>
2025-06-01 19:05:55 -0300

    wip: summary to routes

--- src/main/java/com/swap/api/openapi/AuthControllerOpenApi.java
+++ src/main/java/com/swap/api/openapi/AuthControllerOpenApi.java
@@ -1,11 +1,18 @@
 package com.swap.api.openapi;
 
+import com.swap.api.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Tag(name = "Auth")
 public interface AuthControllerOpenApi {
+    @Operation(summary = "login manager to managers routes")
     ResponseEntity<UsernamePasswordAuthenticationToken> login();
+    @Operation(summary = "register manager to managers routes")
     ResponseEntity<UsernamePasswordAuthenticationToken> register();
 }
