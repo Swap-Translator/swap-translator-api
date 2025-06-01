c7c7372175d06f810971ba94adc110fe7d4ec810
albqvictor1508 <albq.victor@gmail.com>
2025-06-01 08:36:21 -0300

    wip: auth manager to login logic

--- src/main/java/com/swap/api/controller/AuthController.java
+++ src/main/java/com/swap/api/controller/AuthController.java
@@ -1,7 +1,11 @@
 package com.swap.api.controller;
 
+import com.swap.api.dto.AuthDTO;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    @PostMapping
-    public ResponseEntity<?> login(@RequestBody @Valid ) {
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
+    @PostMapping
+    public ResponseEntity<?> login(@RequestBody @Valid AuthDTO request) {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(request.getLogin(), request.getPassword());
     }
 
 }
