1b98e2cbd06616014d9439861f2a2e2f6a42b98d
albqvictor1508 <albq.victor@gmail.com>
2025-06-01 11:12:33 -0300

    wip: trying unlock auth route

--- src/main/java/com/swap/api/controller/AuthController.java
+++ src/main/java/com/swap/api/controller/AuthController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    @Autowired
     private UserRepository repository;
     @Autowired
     private AuthenticationManager authenticationManager;
