1cb21df4df45d93387f254f86d992497b876a521
albqvictor1508 <albq.victor@gmail.com>
2025-06-01 19:08:30 -0300

    wip: deepl translator docs

--- src/main/java/com/swap/api/openapi/AuthControllerOpenApi.java
+++ src/main/java/com/swap/api/openapi/AuthControllerOpenApi.java
@@ -1,15 +1,11 @@
 package com.swap.api.openapi;
 
-import com.swap.api.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Tag(name = "Auth")
+@Tag(name = "Auth Controller - Manager")
 public interface AuthControllerOpenApi {
     @Operation(summary = "login manager to managers routes")
     ResponseEntity<UsernamePasswordAuthenticationToken> login();
--- src/main/java/com/swap/api/openapi/DeeplTranslatorControllerOpenApi.java
+++ src/main/java/com/swap/api/openapi/DeeplTranslatorControllerOpenApi.java
@@ -1,7 +1,11 @@
 package com.swap.api.openapi;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name = "Deepl Controller - Translate")
 public interface DeeplTranslatorControllerOpenApi {
+    @Operation(summary = "translate the text sended")
     ResponseEntity<String> translate();
 }
