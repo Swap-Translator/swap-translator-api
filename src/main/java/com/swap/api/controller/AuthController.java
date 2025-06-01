c947489a8f7a6e3d7d6fe0f1bf394258d3feb65b
albqvictor1508 <albq.victor@gmail.com>
2025-06-01 10:07:53 -0300

    wip: trying to create manager auth route

--- src/main/java/com/swap/api/controller/AuthController.java
+++ src/main/java/com/swap/api/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.swap.api.controller;
 
 import com.swap.api.dto.AuthDTO;
 import com.swap.api.entity.User;
+import com.swap.api.repository.UserRepository;
 import com.swap.api.types.UserRole;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,15 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
     @Autowired
+    private UserRepository repository;
+    @Autowired
     private AuthenticationManager authenticationManager;
     @Autowired
     private PasswordEncoder passwordEncoder;
 
     @PostMapping
     public ResponseEntity<?> login(@RequestBody @Valid AuthDTO request) {
-        User manager = new User();
+        User manager = repository.findByLogin("MANAGER");
+        var passwordEncoded = passwordEncoder.encode(request.getPassword());
         manager.setRole(UserRole.MANAGER);
-        if(!request.getLogin().equals("MANAGER")) return ResponseEntity.badRequest().body("wrong login");
+        if(!request.getLogin().equals("MANAGER")) return ResponseEntity.badRequest().body("wrong login, only MANAGER here");
+        if(!manager.getPassword().equals(passwordEncoded)) return ResponseEntity.badRequest().body("wrong password");
         var usernamePassword = new UsernamePasswordAuthenticationToken(request.getLogin(), request.getPassword());
         authenticationManager.authenticate(usernamePassword);
 
