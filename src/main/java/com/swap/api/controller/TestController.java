6aa16053c7939448c3812288adbfa35ab2ebe9c6
albqvictor1508 <albq.victor@gmail.com>
2025-06-01 13:18:17 -0300

    now i will test

--- src/main/java/com/swap/api/controller/TestController.java
+++ src/main/java/com/swap/api/controller/TestController.java
@@ -3,11 +3,17 @@ package com.swap.api.controller;
 import com.swap.api.dto.AuthDTO;
 import com.swap.api.dto.TranslateRequest;
 import com.swap.api.entity.User;
+import com.swap.api.repository.UserRepository;
+import com.swap.api.types.UserRole;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
+    @Autowired
+    private UserRepository repository;
+
     @GetMapping("/{anything}")
     public String ping(@PathVariable String anything) {
         return anything;
@@ -16,6 +22,7 @@ public class TestController {
     @PostMapping
     public User save(AuthDTO request) {
         User user = new User(request.getLogin(), request.getPassword());
-        user.
+        user.setRole(UserRole.USER);
+        return repository.save(user);
     }
 }
